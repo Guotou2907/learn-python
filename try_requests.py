1ee4ed2c79d846975f0836734554dc782fbbde0c
郭宸溪 <Gcx.sjtu@sjtu.edu.cn>
2026-09-26 14:55:45 +0800

    爬虫的第一次尝试

--- try_requests.py
+++ try_requests.py
@@ -1,3 +1,16 @@
 import requests
-response = requests.get("https://www.baidu.com")
-print(response.status_code)
+from bs4 import BeautifulSoup
+
+response = requests.get("https://baidu.com")
+response.encoding = "utf-8"
+
+soup = BeautifulSoup(response.text,"html.parser")
+
+title = soup.title.string
+print(f"网页标题是：{title}")
+
+links = soup.find_all("a")
+print(f"这个页面有{len(links)}个超链接")
+
+for link in links[:3]:
+    print(f"连接文字是：{link.text}")
